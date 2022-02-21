1e78d8d1af520b35ecd83db30b6732d0a87fbad2
Yeshu Wahane <yeshuwahane@Yeshus-MacBook-Pro.local>
2022-02-21 17:43:39 +0530

    added documentation and fixed UI

--- app/src/main/java/com/alien/assignment/AddStudentActivity.kt
+++ app/src/main/java/com/alien/assignment/AddStudentActivity.kt
@@ -1,23 +1,26 @@
 package com.alien.assignment
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.text.InputType
-import android.widget.ArrayAdapter
-import com.alien.assignment.databinding.ActivityMainBinding
-import android.widget.Toast
-
+import android.provider.MediaStore
 import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.alien.assignment.databinding.ActivityMainBinding
 import com.alien.assignment.model.Student
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import java.io.IOException
 
 
 class AddStudentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
     private var selectedGender = ""
+    private var filePath: Uri? = null
+    val PICK_IMAGE_REQUEST = 21
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +28,19 @@ class AddStudentActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Dropdown menu(Gender selection)
-
         val genders = resources.getStringArray(R.array.Gender)
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_itemview, genders)
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
-
 
         binding.autoCompleteTextView.setOnItemClickListener(OnItemClickListener { parent, view, position, id ->
             selectedGender = genders[position]
         })
 
+
+        //profile picture selection
+        binding.profileImage.setOnClickListener {
+            selectImage()
+        }
 
 
         //firebase
@@ -45,26 +51,24 @@ class AddStudentActivity : AppCompatActivity() {
             val birthDate = binding.etBirthday.text.toString()
             val gender = selectedGender
 
-
             if (studentName.isEmpty() || emailId.isEmpty() || birthDate.isEmpty() || gender.isNullOrBlank()) {
                 Toast.makeText(this, "Please fill the all information", Toast.LENGTH_SHORT).show()
             } else {
 
-                database = FirebaseDatabase.getInstance().getReference("Students")
+                database = FirebaseDatabase.getInstance()
+                    .getReference("Students") // storing the data using path as reference to the table of firebase database
                 val students = Student(studentName, emailId, birthDate, gender)
                 database.child(studentName).setValue(students).addOnSuccessListener {
                     Toast.makeText(this, "Data saved Successful", Toast.LENGTH_SHORT).show()
 
+                    // After data successfully saved clear all the editText Fields
                     binding.etStudentName.text.clear()
                     binding.etEmailId.text.clear()
                     binding.etBirthday.text.clear()
                     binding.autoCompleteTextView.text.clear()
 
-
-                    //scope Function for intent
-                    Intent(this, ShowStudentActivity::class.java).apply {
-                        startActivity(this)
-                    }
+                    val intent = Intent(this, ShowStudentActivity::class.java)
+                    startActivity(intent)
 
 
                 }.addOnFailureListener {
@@ -73,9 +77,50 @@ class AddStudentActivity : AppCompatActivity() {
                 }
             }
         }
+    }
 
+    private fun uploadImage() {
+        /**
+         * TODO upload image in firebase
+         * */
+    }
 
+    //function for selecting the image from file storage
+    private fun selectImage() {
+
+        // Intent for getting the image type data using startActivityForResult method
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+            Intent.createChooser(
+                intent,
+                "Select Image from here..."
+            ),
+            PICK_IMAGE_REQUEST
+        )
     }
 
 
+    //getting image path setting the image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+
+            // Get the path Uri of data
+            filePath = data?.data
+            try {
+
+                // Setting image on image view using Bitmap
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                binding.profileImage.setImageBitmap(bitmap)
+
+            } catch (e: IOException) {
+                // Log the exception
+                e.printStackTrace()
+            }
+        }
+
+    }
 }
